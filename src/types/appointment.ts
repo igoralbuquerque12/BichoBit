27c8730c194aea689814243e1c2bd5e119b03103
Igor <igorcostalins2005@gmail.com>
2025-05-31 01:46:04 -0300

    feat: create appointments interface

---
+++ src/types/appointment.ts
@@ -0,0 +1,12 @@
+export interface Appointment {
+  id: number;
+  animalName: string;
+  breed: string;
+  weight: number;
+  service: string;
+  ownerName: string;
+  contact: string;
+  scheduledAt: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
